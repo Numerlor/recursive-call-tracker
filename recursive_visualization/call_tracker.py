ecf29948c7821068601fb907720cacfb9caa8847
Numerlor <Numerlor@gmail.com>
2022-07-16 14:45:12 +0200

    add call tracker

---
+++ recursive_visualization/call_tracker.py
@@ -0,0 +1,66 @@
+import enum
+import typing as t
+from collections import abc
+from functools import wraps
+
+P = t.ParamSpec("P")
+R = t.TypeVar("R")
+
+
+class Uninitialized(enum.Enum):  # noqa: D101
+    UNINITIALIZED = enum.auto()
+
+    def __str__(self):
+        return self._name_
+    __repr__ = __str__
+
+
+UNINITIALIZED = Uninitialized.UNINITIALIZED
+
+
+class RecursiveCall:
+    """
+    A single recursive call.
+
+    Recursive calls from within this call should be added to `self.child_calls`.
+    """
+    def __init__(self, args: tuple[object, ...], kwargs: dict[str, object]):
+        self.child_calls = list[RecursiveCall]()
+        self.args = args
+        self.kwargs = kwargs
+        self.result: object | t.Literal[UNINITIALIZED] = UNINITIALIZED
+
+    def __repr__(self):
+        return f"<RecursiveCall child_calls={self.child_calls} args={self.args} kwargs={self.kwargs} result={self.result})"
+
+
+class CallTracker:
+    """
+    Track all recursive calls of the wrapped function.
+
+    The initial call for each recursive chain is stored in the `start_calls` attribute.
+    """
+    def __init__(self):
+        self._active_calls = list[RecursiveCall]()
+        self.start_calls = list[RecursiveCall]()
+
+    def __call__(self, func: abc.Callable[P, R]) -> abc.Callable[P, R]:
+
+        @wraps(func)
+        def wrapper(*args: P.args, **kwargs: P.kwargs) -> R:
+            call = RecursiveCall(args, kwargs)
+
+            if self._active_calls:
+                self._active_calls[-1].child_calls.append(call)
+            else:
+                self.start_calls.append(call)
+
+            self._active_calls.append(call)
+
+            result = func(*args, **kwargs)
+
+            self._active_calls.pop().result = result
+
+            return result
+
+        return wrapper
